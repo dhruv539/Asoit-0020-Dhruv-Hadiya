23f42b3ebbc85fdb0c9de3523ba37ccf1c17426b
dhruv539 <83349044+dhruv539@users.noreply.github.com>
2023-06-29 20:47:00 +0530

    Delete day 6 directory

--- day 6/myjs.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("click").addEventListener("click",displaylenght)
-
-function displaylenght(){
-    var name=document.getElementById("getdata").value;
-    document.getElementById("showop").innerText=name.length;
-}
